14a331484ec719488d5709e61ac455e387011024
Roberto <rabustamante5@utpl.edu.ec>
2022-12-21 09:51:14 -0500

    avance del trabajo en clase de la semana 3 del dia martes

--- Ejemplosc3/src/paquete01/Ejemplo01.java
+++ Ejemplosc3/src/paquete01/Ejemplo01.java
@@ -22,7 +22,9 @@ public class Ejemplo01 {
         
         // forma 2
         int[][] arreglo2 = {{10, 20, 30}, {100, 200, 300}};
-        
+        // Cadena
+        String[][] arreglo5 = {{"Ana","Jose","Pedro"},
+                               {"Luis","Maria","Juan"}};
         // forma 3
         int[][] arreglo3 = new int[3][];
         arreglo3[0] = new int[2];
