8baafa0dabf9eb7eafad7339689a370564443ba0
Roberto <rabustamante5@utpl.edu.ec>
2022-12-21 10:53:36 -0500

    Trabajo en clase semana 3 dia miercoles final

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -18,11 +18,33 @@ public class Ejemplo2 {
         // Creación de arreglo bidimensionales
         
         double[][] dato1 = {{1, 2, 3},{6, 8, 9}}; //
+        double[][] mensaje = new double[2][3] ;
+        double valor;
+        
         /*
             1   4   3
             36  64  9
         */
-        
+        for (int i = 0; i < dato1.length; i++) {
+            valor = 0;
+            for (int j = 0; j < dato1[i].length; j++) {
+                if (dato1[i][j]%2 == 0){
+                    valor = dato1[i][j] * dato1[i][j];
+                mensaje[i][j] = valor;
+                
+            }else{
+                valor = dato1[i][j];
+                mensaje[i][j] = valor;
+                }}
+            
     }
-    
+        for (int i = 0; i < dato1.length; i++) {
+            
+            for (int j = 0; j < dato1[i].length; j++) {
+                System.out.printf("%.0f\t",mensaje[i][j]);
+            }
+            System.out.println();
+                }
+    //Roberto Bustamante 
+}
 }
